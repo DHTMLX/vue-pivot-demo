c684b0a7ff6eb76bdc37e5c6466fafff949f0c1d
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-09-17 22:45:03 +0400

    [dev] refactor

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Pivot from "./components/Pivot.vue";
+import { getData } from "./data";
+
+export default {
+  components: { Pivot },
+  data() {
+    const { fields, dataset } = getData();
+    return {
+      fields,
+      dataset
+    };
+  }
+};
+</script>
+
+<template>
+  <Pivot :fields="fields" :dataset="dataset" />
+</template>

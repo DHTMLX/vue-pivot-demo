eaaddab82ea5808768f51801295a7c79d5a03ca4
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-09-17 22:40:49 +0400

    [delete] clear prev code and files

---
+++ src/components/Pivot.vue
@@ -0,0 +1,58 @@
+<script>
+import { Pivot } from "@dhx/trial-pivot";
+import "@dhx/trial-pivot/dist/pivot.css";
+
+export default {
+    props: ["fields", "dataset"],
+
+    mounted() {
+        this.table = new Pivot(this.$refs.container, {
+            fields: this.fields,
+            data: this.dataset,
+            headerShape: {
+                collapsible: true
+            },
+            tableShape: {
+                tree: true,
+                templates: ["profit", "sales"].reduce((acc, field) => (acc[field] = v => (v ? Math.ceil(v) : ""), acc), {})
+            },
+            columnShape: {
+                autoWidth: {
+                    columns: {
+                        type: true,
+                        title: true
+                    },
+                    firstOnly: false
+                }
+            },
+            config: {
+                rows: ["state", "product_type"],
+                columns: ["product_line", "type"],
+                values: [
+                    {
+                        field: "profit",
+                        method: "sum"
+                    },
+                    {
+                        field: "sales",
+                        method: "sum"
+                    },
+                    {
+                        field: "date",
+                        method: "min"
+                    }
+                ]
+            }
+            // other configuration properties
+        });
+    },
+
+    unmounted() {
+        this.table.destructor();
+    }
+};
+</script>
+
+<template>
+    <div ref="container" class="widget"></div>
+</template>

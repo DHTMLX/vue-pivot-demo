912f883ebec7a472f6b36a0e8a6e3675969c9172
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-09-18 22:24:45 +0400

    [dev] add the required property (auto)

--- src/components/Pivot.vue
+++ src/components/Pivot.vue
@@ -18,6 +18,7 @@ export default {
             },
             columnShape: {
                 autoWidth: {
+                    auto: true,
                     columns: {
                         type: true,
                         title: true
